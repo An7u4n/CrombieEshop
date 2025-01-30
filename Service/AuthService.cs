2d56147cab9100a1a9f99ebab6be3884f227326a
Bernard Maximiliano <bmaximiliano0210@gmail.com>
2025-01-30 01:15:26 -0300

    refactor: CognitoAuthService uses IAuthService

--- Service/AuthService.cs
+++ Service/AuthService.cs
@@ -12,50 +12,26 @@ namespace Service
     public class AuthService : IAuthService
     {
         private readonly IUsuarioRepository _userRepository;
-        private readonly ICognitoAuthService _cognitoAuthService;
         private readonly PasswordHasher<Usuario> _passwordHasher = new PasswordHasher<Usuario>();
-        public AuthService(IUsuarioRepository usuarioRepository,ICognitoAuthService cognitoAuthService)
+        public AuthService(IUsuarioRepository usuarioRepository)
         {
             _userRepository = usuarioRepository;
-            _cognitoAuthService = cognitoAuthService;
-        }
-        // Should fetch user via email/username and password
-        public UsuarioDTO LoginUsuario(AuthDTO userData)
-        {
-
-            Usuario user = null;
-            if (userData.Email != null)
-            {
-                user = _userRepository.FindByEmail(userData.Email);
-            }
-            else if (userData.NombreUsuario != null)
-            {
-                user = _userRepository.FindByNombreUsuario(userData.NombreUsuario);
-            }
-            PasswordVerificationResult result = _passwordHasher.VerifyHashedPassword(user, user.Contrasena, userData.Contrasena);
-            if (result == PasswordVerificationResult.Failed)
-            {
-                throw new Exception("Invalid password");
-            }
-
-            return new UsuarioDTO(user);
         }
 
-        public async Task<UsuarioDTO> RegistrarUsuario(UsuarioDTO userData)
+        public Task ConfirmarRegistroAsync(string userName, string code)
         {
-            try
-            {
-                Usuario user = new Usuario(userData);
-                await _cognitoAuthService.RegistrarAsync(userData.Email, userData.Contrasena);
-                _userRepository.CrearUsuario(user);
-                var newUser = new UsuarioDTO(user);
-                newUser.Contrasena = null;
-                return newUser;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            throw new NotImplementedException();
+        }
+
+
+        public Task<string> IniciarSesion(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RegistrarAsync(string email, string password)
+        {
+            throw new NotImplementedException();
         }
     }
 }
